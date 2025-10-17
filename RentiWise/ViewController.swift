612d66b218ee64da1e0a530c1afc7b75b90eeb7f
Aditya yadav <aditya112004ahir@gmail.com>
2025-10-18 03:57:43 +0530

    Initial Commit

---
+++ RentiWise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RentiWise
+//
+//  Created by admin99 on 18/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RentiWiseTests/RentiWiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  RentiWiseTests.swift
+//  RentiWiseTests
+//
+//  Created by admin99 on 18/10/25.
+//
+
+import Testing
+@testable import RentiWise
+
+struct RentiWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
