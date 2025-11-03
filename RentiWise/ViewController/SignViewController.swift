10d598d2097bd5af93300b8ac26ba678ee9552d8
Aditya yadav <aditya112004ahir@gmail.com>
2025-11-03 10:22:49 +0530

    added new storyboard and some changes

--- RentiWise/ViewController/SignViewController.swift
+++ RentiWise/ViewController/SignViewController.swift
@@ -11,19 +11,19 @@ class SignViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
     
-   
+    @IBAction func forgotPassword(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "forgotPassword")
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Configure destination if needed.
     }
     */
-
 }
