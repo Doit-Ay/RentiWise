705e1829c8f20274cbe701112e6ae3bb34b6cda4
Aditya yadav <aditya112004ahir@gmail.com>
2025-11-08 22:05:58 +0530

    updated main from Aditya branch

--- RentiWise/ViewController/DashboardViewController.swift
+++ RentiWise/ViewController/DashboardViewController.swift
@@ -52,11 +52,6 @@ class DashboardViewController: UIViewController, UITabBarDelegate {
     /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
